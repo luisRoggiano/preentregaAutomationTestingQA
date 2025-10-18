df9639b0a7ada732b4e16c02e41f661c7b19540b
luisRoggiano <luis.roggiano@gmail.com>
2025-10-18 13:32:59 -0300

    [20251018] [AutomationQA]
    the following files were added/updated in order to apply screenshots test evidence
            modified:   test/__pycache__/test_saucedemo.cpython-313-pytest-8.4.2.pyc
            new file:   test/screenshots/carritoAddOk_2025_10_18_13-29-26.png
            new file:   test/screenshots/carritoRemoveOK_2025_10_18_13-29-26.png
            new file:   test/screenshots/catalogoOk_2025_10_18_13-29-26.png
            new file:   test/screenshots/loginOk_2025_10_18_13-29-26.png
            modified:   test/test_saucedemo.py
            modified:   utils/__pycache__/helpers.cpython-313.pyc
            modified:   utils/helpers.py

--- test/test_saucedemo.py
+++ test/test_saucedemo.py
@@ -2,9 +2,10 @@ import pytest                                                                   
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))          #__file__ variable de la ruta del archivo actual, por que sería mediante el join path&.. abspath pasa de ruta relativa a ruta absoluta
-from utils.helpers import login_saucedemo, get_driver
+from utils.helpers import login_saucedemo, get_driver, take_screenshot
 from selenium.webdriver.common.by import By
 import time
+
 
 
 @pytest.fixture(scope='session')                                                        #pytest.fixture(scope='session') mantiene la sesión iniciada mientras se ejecutan los test
@@ -14,23 +15,18 @@ def driver():
     yield driver                                                                        #palabra reservada yield, le da al navegador el driver correcto. manteniendo el navegador abierto similar a scope
     #driver.quit()                                                                       #cerramos la ventana
 
-
-
 def test_login(driver):
     login_saucedemo(driver)
     assert "/inventory.html" in driver.current_url                                      #verifica que estemos en la pagina correcta de inventory
     titulo = driver.find_element(By.CLASS_NAME, 'app_logo').text                        #verificamos el título
-    assert titulo == 'Swag Labs'                                                        #verificamos que diga Swag Labs en el logo
-
-
-
-#def test_catalogo():
+    assert titulo == 'Swag Labs'
+    take_screenshot(driver, "loginOk", __file__)                                                        #verificamos que diga Swag Labs en el logo
 
 def test_catalogo(driver):
     #login_saucedemo(driver)
     products = driver.find_elements(By.CLASS_NAME, 'inventory_list')                    #usamos findelements por que son muchos elementos
     assert len(products) > 0                                                            #verificamos que los elementos esten contenidos dentro del main container
-    
+    take_screenshot(driver, "catalogoOk", __file__) 
 def test_carrito(driver):
     #login_saucedemo(driver)
     products = driver.find_elements(By.CLASS_NAME, 'inventory_item')
@@ -42,7 +38,10 @@ def test_carrito(driver):
     #verificamos que el carrito agrega los productos
     badge = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
     assert int(badge) == 3
+    take_screenshot(driver, "carritoAddOk", __file__) 
+    
     products[0].find_element(By.TAG_NAME, 'button').click()
     #verificamos que el arrito elimina productos
     badge = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
     assert int(badge) == 2
+    take_screenshot(driver, "carritoRemoveOK", __file__)
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,17 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+import os
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-
+from datetime import datetime 
 
 URL = 'https://www.saucedemo.com/'
 USERNAME = 'standard_user'
 PASSWORD = 'secret_sauce'
-
+SCREENSHOT_PATH = r'E:\curso-automation-testing\preentregaAutomationTestingQA\test\screenshots'
 
 def get_driver():
 
@@ -30,7 +30,21 @@ def login_saucedemo(driver):
     time.sleep(1)
     driver.find_element(By.ID,'login-button').click()
     time.sleep(5)
-        
+
+def take_screenshot(driver, test_name, test_file_path):
+    test_dir = os.path.dirname(os.path.abspath(test_file_path))
+    screenshot_dir = os.path.join(test_dir, "screenshots")
+    if not os.path.exists(screenshot_dir):
+        os.makedirs(screenshot_dir, exist_ok=True)
+    timestamp = datetime.now().strftime("%Y_%m_%d_%H-%M-%S")
+    filename = f"{test_name}_{timestamp}.png"
+    filepath = os.path.join(screenshot_dir, filename)
+    try:
+        driver.save_screenshot(filepath)
+        print(f"\n✅ Captura guardada en: {filepath}")
+    except Exception as e:
+        print(f"\n❌ Error al guardar la captura de pantalla: {e}")
+
 
 
 
